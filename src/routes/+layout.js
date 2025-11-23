7c331aeb6174313b7e32da3f609e70158d63300b
Alfayed Dennita <alfayeddennita@gmail.com>
2025-11-23 23:41:43 +0700

    Add metadata

---
+++ src/routes/+layout.js
@@ -0,0 +1,10 @@
+export function load() {
+  const metadata = {
+    title: 'Dismantled Spike - Simplified One-Stop Valorant Wiki',
+    description:
+      'Dismantled Spike is a simplified one-stop Valorant wiki that contains essential Valorant information to boost your game knowledge. Find information about Valorant agents, maps, weapons, and more easily in Dismantled Spike. All Valorant data on this web app is obtained from Valorant-API (valorant-api.com) as a public API provider.',
+    keywords: ['Dismantled Spike', 'Valorant', 'valo', 'game', 'wiki', 'guide'],
+  };
+
+  return { metadata };
+}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,61 @@
 <script>
-  import favicon from '$lib/assets/favicon.svg';
+  import { onMount } from 'svelte';
+  import { page } from '$app/state';
   import './layout.css';
 
-  let { children } = $props();
+  const { children } = $props();
+
+  const { title, description, keywords } = $derived(page.data.metadata);
+  let theme = $state();
+
+  function handleRefreshingTheme({ detail: { localStorageTheme } }) {
+    theme = localStorageTheme;
+  }
+
+  onMount(() => {
+    theme = window.theme.theme.localStorageTheme;
+    window.theme.addEventListener('refresh', handleRefreshingTheme);
+
+    return () => {
+      window.theme.removeEventListener('refresh', handleRefreshingTheme);
+    };
+  });
 </script>
 
 <svelte:head>
-  <link rel="icon" href={favicon} />
+  {#if description}
+    <meta name="description" content={description} />
+  {/if}
+  {#if keywords}
+    <meta name="keywords" content={Array.from(new Set(keywords)).join(', ')} />
+  {/if}
+  {#if title}
+    <title>{title}</title>
+  {/if}
 </svelte:head>
+
+<p>Theme: {theme || 'System Preference'}</p>
+<button
+  class="btn btn-primary"
+  type="button"
+  onclick={() => (window.theme.theme = 'light')}
+>
+  Change to Light
+</button>
+<button
+  class="btn btn-secondary"
+  type="button"
+  onclick={() => (window.theme.theme = 'dark')}
+>
+  Change to Dark
+</button>
+<button
+  class="btn btn-accent"
+  type="button"
+  onclick={() => (window.theme.theme = null)}
+>
+  Change to System Preference
+</button>
+<a class="btn btn-neutral" href="/demo">Demo</a>
 
 {@render children()}
