2242dd881b50b5e70c998ae092ce9a76d3e863fe
Alfayed Dennita <alfayeddennita@gmail.com>
2025-11-29 17:31:04 +0700

    Fix container

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,74 +2,76 @@
   const { data } = $props();
 </script>
 
-<div
-  class="mx-auto flex w-full max-w-7xl flex-col justify-between py-4 md:gap-4 md:px-4 md:py-8 lg:flex-row lg:gap-8 lg:p-12 xl:px-0"
->
-  <main class="lg:w-8/12">
-    <section class="flex flex-col gap-4 p-4 md:bg-base-200 md:p-8">
-      <h3 class="text-lg font-bold uppercase">Categories</h3>
-      <ul class="grid gap-4 sm:grid-cols-2">
-        {#each data.categories as category (category.name)}
-          <li class="contents">
-            <svelte:element
-              this={category.url ? 'a' : 'div'}
-              class={[
-                'group @container flex h-20 items-stretch bg-primary contrast-more:text-primary-content',
-                category.url
-                  ? 'text-primary-content'
-                  : 'text-primary-content/60',
-                !category.url && 'select-none',
-              ]}
-              href={category.url}
-              title={category.url ? category.name : undefined}
-            >
-              <span
-                class="flex grow flex-col justify-center p-4 @sm:flex-row @sm:items-center @sm:justify-start @sm:gap-1 @sm:px-6"
+<div class="md:px-4 lg:px-12">
+  <div
+    class="mx-auto flex max-w-7xl flex-col justify-between py-4 md:gap-4 md:py-8 lg:flex-row lg:gap-8 lg:py-12"
+  >
+    <main class="lg:w-8/12">
+      <section class="flex flex-col gap-4 p-4 md:bg-base-200 md:p-8">
+        <h3 class="text-lg font-bold uppercase">Categories</h3>
+        <ul class="grid gap-4 sm:grid-cols-2">
+          {#each data.categories as category (category.name)}
+            <li class="contents">
+              <svelte:element
+                this={category.url ? 'a' : 'div'}
+                class={[
+                  'group @container flex h-20 items-stretch bg-primary contrast-more:text-primary-content',
+                  category.url
+                    ? 'text-primary-content'
+                    : 'text-primary-content/60',
+                  !category.url && 'select-none',
+                ]}
+                href={category.url}
+                title={category.url ? category.name : undefined}
               >
-                <span class="flex items-center gap-1 uppercase">
-                  <span class={['mb-1 iconify', category.icon]}></span>
-                  {category.name}
-                </span>
-                {#if !category.url}
-                  <span
-                    class="text-xs text-primary-content/40 italic contrast-more:text-primary-content"
-                    >(Coming Soon)</span
-                  >
-                {/if}
-              </span>
-              <span class="relative">
-                <img
-                  class="h-full w-24 object-cover object-center @xs:w-32"
-                  src={category.image}
-                  alt={category.name}
-                />
-                {#if category.url}
-                  <span
-                    class="invisible absolute inset-0 z-10 flex w-0 items-center justify-center gap-1 overflow-hidden bg-secondary text-sm font-bold text-secondary-content uppercase transition-all group-hover:visible group-hover:w-full motion-reduce:transition-none"
-                  >
-                    Open
-                    <span class="mb-1 iconify famicons--open-sharp"></span>
+                <span
+                  class="flex grow flex-col justify-center p-4 @sm:flex-row @sm:items-center @sm:justify-start @sm:gap-1 @sm:px-6"
+                >
+                  <span class="flex items-center gap-1 uppercase">
+                    <span class={['mb-1 iconify', category.icon]}></span>
+                    {category.name}
                   </span>
-                {/if}
-              </span>
-            </svelte:element>
-          </li>
-        {/each}
-      </ul>
-    </section>
-  </main>
-  <aside class="lg:w-4/12">
-    <section class="flex flex-col gap-4 p-4 md:bg-base-200 md:p-8">
-      <h3 class="text-lg font-bold uppercase">Your Recent Visits</h3>
-      <ul class="menu w-full bg-base-200 md:bg-base-300">
-        {#each data.recentVisits as recentVisit (recentVisit.name)}
-          <li>
-            <a href={recentVisit.url} title={recentVisit.name}>
-              {recentVisit.name}
-            </a>
-          </li>
-        {/each}
-      </ul>
-    </section>
-  </aside>
+                  {#if !category.url}
+                    <span
+                      class="text-xs text-primary-content/40 italic contrast-more:text-primary-content"
+                      >(Coming Soon)</span
+                    >
+                  {/if}
+                </span>
+                <span class="relative">
+                  <img
+                    class="h-full w-24 object-cover object-center @xs:w-32"
+                    src={category.image}
+                    alt={category.name}
+                  />
+                  {#if category.url}
+                    <span
+                      class="invisible absolute inset-0 z-10 flex w-0 items-center justify-center gap-1 overflow-hidden bg-secondary text-sm font-bold text-secondary-content uppercase transition-all group-hover:visible group-hover:w-full motion-reduce:transition-none"
+                    >
+                      Open
+                      <span class="mb-1 iconify famicons--open-sharp"></span>
+                    </span>
+                  {/if}
+                </span>
+              </svelte:element>
+            </li>
+          {/each}
+        </ul>
+      </section>
+    </main>
+    <aside class="lg:w-4/12">
+      <section class="flex flex-col gap-4 p-4 md:bg-base-200 md:p-8">
+        <h3 class="text-lg font-bold uppercase">Your Recent Visits</h3>
+        <ul class="menu w-full bg-base-200 md:bg-base-300">
+          {#each data.recentVisits as recentVisit (recentVisit.name)}
+            <li>
+              <a href={recentVisit.url} title={recentVisit.name}>
+                {recentVisit.name}
+              </a>
+            </li>
+          {/each}
+        </ul>
+      </section>
+    </aside>
+  </div>
 </div>
--- src/routes/agents/+page.svelte
+++ src/routes/agents/+page.svelte
@@ -2,47 +2,47 @@
   const { data } = $props();
 </script>
 
-<main
-  class="mx-auto flex max-w-7xl flex-col gap-8 px-4 py-8 lg:gap-12 lg:px-12 lg:py-16 xl:px-0"
->
-  <h1
-    class=" text-center text-2xl font-bold uppercase lg:text-3xl lg:font-extrabold"
-  >
-    Agents
-  </h1>
-  <div class="flex flex-wrap justify-center gap-8">
-    {#each data.agents as agent (agent.id)}
-      <article
-        class="card grow bg-base-200 sm:max-w-[calc(100%/2-var(--spacing)*8/2)] md:max-w-[calc(100%/3-var(--spacing)*16/3)] lg:max-w-[calc(100%/3-var(--spacing)*16/3)] xl:max-w-[calc(100%/4-var(--spacing)*24/4)]"
-      >
-        <a
-          class="group"
-          href="/agents/{agent.id}"
-          title="{agent.name} ({agent.role.name})"
+<main class="px-4 lg:px-12">
+  <div class="mx-auto flex max-w-7xl flex-col gap-8 py-8 lg:gap-12 lg:py-16">
+    <h1
+      class=" text-center text-2xl font-bold uppercase lg:text-3xl lg:font-extrabold"
+    >
+      Agents
+    </h1>
+    <div class="flex flex-wrap justify-center gap-8">
+      {#each data.agents as agent (agent.id)}
+        <article
+          class="card grow bg-base-200 sm:max-w-[calc(100%/2-var(--spacing)*8/2)] md:max-w-[calc(100%/3-var(--spacing)*16/3)] lg:max-w-[calc(100%/3-var(--spacing)*16/3)] xl:max-w-[calc(100%/4-var(--spacing)*24/4)]"
         >
-          <figure
-            class="relative before:absolute before:inset-x-0 before:bottom-0 before:h-0 before:bg-base-300 before:transition-[height] group-hover:before:h-full motion-reduce:before:transition-none"
+          <a
+            class="group"
+            href="/agents/{agent.id}"
+            title="{agent.name} ({agent.role.name})"
           >
-            <img
-              class="z-10"
-              src={agent.images.portrait}
-              alt="{agent.name} Portrait"
-            />
-          </figure>
-          <div class="card-body bg-primary text-primary-content">
-            <h2 class="card-title text-xl uppercase">
+            <figure
+              class="relative before:absolute before:inset-x-0 before:bottom-0 before:h-0 before:bg-base-300 before:transition-[height] group-hover:before:h-full motion-reduce:before:transition-none"
+            >
               <img
-                class="mb-1 h-4"
-                src={agent.role.icon}
-                alt={agent.role.name}
+                class="z-10"
+                src={agent.images.portrait}
+                alt="{agent.name} Portrait"
               />
-              <span>
-                {agent.name}
-              </span>
-            </h2>
-          </div>
-        </a>
-      </article>
-    {/each}
+            </figure>
+            <div class="card-body bg-primary text-primary-content">
+              <h2 class="card-title text-xl uppercase">
+                <img
+                  class="mb-1 h-4"
+                  src={agent.role.icon}
+                  alt={agent.role.name}
+                />
+                <span>
+                  {agent.name}
+                </span>
+              </h2>
+            </div>
+          </a>
+        </article>
+      {/each}
+    </div>
   </div>
 </main>
