5e05e702bab9473eb68590776ac32b638d8b39d0
Alfayed Dennita <alfayeddennita@gmail.com>
2025-11-29 17:19:12 +0700

    Add agents metadata

--- src/routes/agents/+page.js
+++ src/routes/agents/+page.js
@@ -1,7 +1,19 @@
 import db from '$lib/db';
 
-export async function load({ fetch }) {
+export async function load({ parent, fetch }) {
   const agents = await db.agents.getAll(fetch);
+  const parentData = await parent();
+  const metadata = {
+    title: 'Agents - Dismantled Spike',
+    description:
+      'Agents are the playable characters in VALORANT, representing an agent of the VALORANT protocol. Each agent serves as a different role or class with four unique abilities. There are four roles/classes: duelist, initiator, controller, and sentinel.',
+    keywords: [
+      ...parentData.metadata.keywords,
+      'agents',
+      'characters',
+      'heroes',
+    ],
+  };
 
-  return { agents };
+  return { metadata, agents };
 }
